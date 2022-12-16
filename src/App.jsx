669d5f52376107608051b26fb4d52e8670f4d864
ArshFarwaha <arshdeep.farwaha@gmail.com>
2022-12-16 15:28:22 -0500

    Library React Implimentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,74 @@
 import "./index.css";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav.jsx";
 import "./components/Explore";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from "./data";
-
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) =>
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id != item.id))
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
   return (
-      <Router>
-        <div className="App">
-          <Nav />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/books" element={<Books books={books} />} />
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books books={books} />} />
+          <Route
+            path="/books/:id"
+            element={
+              <BookInfo books={books} addToCart={addToCart} cart={cart} />
+            }
+          />
+          <Route
+            path="/cart"
+            element={
+              <Cart
+                books={books}
+                cart={cart}
+                changeQuantity={changeQuantity}
+                removeItem={removeItem}
+              />
+            }
+          />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+// start video at 1:56
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+const Cart = ({ cart, changeQuantity, removeItem }) => {
+  const total = () => {
+    let price = 0;
+    cart.forEach((item) => {
+      price += +(item.salePrice || item.originalPrice * item.quantity);
+    });
+    return price;
+  };
+
+  return (
+    <div id="books__body">
+      <div className="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="cart__title">Cart</h2>
+            </div>
+            <div className="cart">
+              <div className="cart__header">
+                <span className="cart__book">Book</span>
+                <span className="cart__quantity">Quantity</span>
+                <span className="cart__total">Price</span>
+              </div>
+            </div>
+            <div className="cart__body">
+              {cart.map((book) => {
+                return (
+                  <div key={book.id} className="cart__item">
+                    <div className="cart__book">
+                      <img src={book.url} alt="" className="cart__book--img" />
+                      <div className="cart__book--info">
+                        <span className="cart__book--title">
+                          Crack the coding interview
+                        </span>
+                        <span className="cart__book--price">
+                          ${(book.salePrice || book.originalPrice).toFixed(2)}
+                        </span>
+                        <button
+                          className="cart__book--remove"
+                          onClick={() => removeItem(book)}
+                        >
+                          Remove
+                        </button>
+                      </div>
+                    </div>
+                    <div className="cart__quantity">
+                      <input
+                        type="number"
+                        min={0}
+                        max={99}
+                        className="cart__input"
+                        value={book.quantity}
+                        onChange={(event) =>
+                          changeQuantity(book, event.target.value)
+                        }
+                      />
+                    </div>
+                    <div className="cart__total">
+                      $
+                      {(
+                        (book.salePrice || book.originalPrice) * book.quantity
+                      ).toFixed(2)}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="total">
+              <div className="total__item total__sub-total">
+                <span>Subtotal</span>
+                <span>${(total() * 0.9).toFixed(2)}</span>
+              </div>
+              <div className="total__item total__sub-tax">
+                <span>Tax</span>
+                <span>${(total() * 0.1).toFixed(2)}</span>
+              </div>
+              <div className="total__item total__sub-price">
+                <span>Total</span>
+                <span>${total().toFixed(2)}</span>
+              </div>
+              <button
+                className="btn btn__checkout no-cursor"
+                onClick={() => alert("Did not get into to doing this")}
+              >
+                Proceed to checkout
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
+
+// start at 2:22
